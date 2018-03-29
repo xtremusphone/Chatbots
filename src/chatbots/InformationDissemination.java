1cb6ee9cb059c479e8c167aab101e80894b5f5c1
xtremusphone <xtremusphone@gmail.com>
2018-03-29 19:05:05 +0800

    Not sure what I've change here lol

--- src/chatbots/InformationDissemination.java
+++ src/chatbots/InformationDissemination.java
@@ -3,8 +3,13 @@ package chatbots;
 import java.util.ArrayList;
 import java.util.HashMap;
 import nlp.Chunker;
+import nlp.WordTokenizer;
 
 public class InformationDissemination {
+    
+    private ArrayList<String> answers;
+    private ArrayList<String> keyword;
+    private ArrayList<String> q_type;
     
     private HashMap<String,String> tagged;
     private ArrayList<String> sentence;
@@ -13,23 +18,92 @@ public class InformationDissemination {
     public InformationDissemination(HashMap<String,String> tag,ArrayList<String> sntc){
         tagged = tag;
         sentence = sntc;
+        answers = new ArrayList<>();
+        keyword = new ArrayList<>();
+        q_type = new ArrayList<>();
         for(String x:sentence){
             sentence_build += x + " ";
         }
         sentence_build = sentence_build.substring(0,sentence_build.length() - 1);
     }
     
-    public void splitSentence(){
+    public ArrayList<String> splitSentence(){
         //splitting sentence
         ArrayList<String> sentence_list = new ArrayList<>();
         mergeNNP();
         if(sentence.contains("and")){
             int and_index = sentence.indexOf("and");
             if(tagged.get(sentence.get(and_index - 1)).equals("NNP") && tagged.get(sentence.get(and_index + 1)).equals("NNP")){
-                //if(tagged.get(and_index) + )
+                String root_sentence = "";
+                String first_sentence = "";
+                String second_sentence = "";
+                System.out.println(and_index + 1);
+                System.out.println(sentence.size());
+                if(and_index + 3 < sentence.size()){                   
+                    for(int i = and_index + 2; i < sentence.size(); i ++){
+                        if(tagged.get(sentence.get(i)).equals("PRP") || tagged.get(sentence.get(i)).equals("PRP$") || tagged.get(sentence.get(i)).equals("PRP")){
+                            continue;
+                        }
+                        root_sentence += sentence.get(i) + " ";
+                    }
+                    root_sentence = root_sentence.substring(0, root_sentence.length() - 1);
+                    first_sentence = sentence.get(and_index - 1) + " " + root_sentence;
+                    sentence_list.add(first_sentence);
+                    second_sentence = sentence.get(and_index + 1) + " " + root_sentence;
+                    sentence_list.add(second_sentence);
+                }
+                else{
+                    for(int i = 0; i < and_index - 1;i++){
+                        if(tagged.get(sentence.get(i)).equals("PRP") || tagged.get(sentence.get(i)).equals("PRP$") || tagged.get(sentence.get(i)).equals("PRP")){
+                            continue;
+                        }
+                        root_sentence += sentence.get(i) + " "; 
+                    }
+                    root_sentence = root_sentence.substring(0, root_sentence.length() - 1);
+                    first_sentence = root_sentence + " " + sentence.get(and_index - 1);
+                    sentence_list.add(first_sentence);
+                    second_sentence = root_sentence + " " + sentence.get(and_index + 1);
+                    sentence_list.add(second_sentence);                 
+                }
+                System.out.println(first_sentence);
+                System.out.println(second_sentence);
+            }
+            
+            if(tagged.get(sentence.get(and_index - 1)).startsWith("VB") && tagged.get(sentence.get(and_index + 1)).startsWith("VB")){
+                String root_sentence = "";
+                String first_sentence = "";
+                String second_sentence = "";
+                System.out.println(and_index + 1);
+                System.out.println(sentence.size());
+                if(and_index + 3 < sentence.size()){                   
+                    for(int i = and_index + 2; i < sentence.size(); i ++){
+                        root_sentence += sentence.get(i) + " ";
+                    }
+                    root_sentence = root_sentence.substring(0, root_sentence.length() - 1);
+                    first_sentence = sentence.get(and_index - 1) + " " + root_sentence;
+                    sentence_list.add(first_sentence);
+                    second_sentence = sentence.get(and_index + 1) + " " + root_sentence;
+                    sentence_list.add(second_sentence);
+                }
+                else{
+                    for(int i = 0; i < and_index - 1;i++){
+                        root_sentence += sentence.get(i) + " "; 
+                    }
+                    root_sentence = root_sentence.substring(0, root_sentence.length() - 1);
+                    first_sentence = root_sentence + " " + sentence.get(and_index - 1);
+                    sentence_list.add(first_sentence);
+                    second_sentence = root_sentence + " " + sentence.get(and_index + 1);
+                    sentence_list.add(second_sentence);                 
+                }               
+                System.out.println(first_sentence);
+                System.out.println(second_sentence);
+            }
+            
+            if(tagged.get(sentence.get(and_index - 1)).equals("NNP") && tagged.get(sentence.get(and_index + 1)).equals("NNP")){
+                
             }
         }
-        
+        return sentence_list;
     }
     
     private void mergeNNP(){
@@ -45,6 +119,39 @@ public class InformationDissemination {
             tagged.put(person, "NNP");
             sentence.add(pointer, person);
         }
-        System.out.println(sentence.toString());
+    }
+    
+    private void disseminateInformation(ArrayList<String> sentence_list){
+        //answers
+        //keyword
+        //q_type
+        WordTokenizer tk = new WordTokenizer();
+        for(String sentence:sentence_list){
+            //who
+            String kw = "";
+            for(String word:tk.tokenizer(sentence)){
+                if(tagged.get(word).equals("NNP"))
+                    answers.add(word);
+                else
+                    kw += word + " ";
+            }
+            keyword.add(kw.substring(0,kw.length() - 1));
+            q_type.add("Who");
+            
+            //what
+            kw = "";
+            for(String word:tk.tokenizer(sentence)){
+                if(tagged.get(word).startsWith("VB"))
+                    answers.add(word);
+                else
+                    kw += word;
+            }
+            keyword.add(kw.substring(0,kw.length() - 1));
+            q_type.add("What");
+        
+            
+            //where
+            //why
+        }
     }
 }
--- src/chatbots/QuestionClassification.java
+++ src/chatbots/QuestionClassification.java
@@ -9,6 +9,24 @@ public class QuestionClassification {
         
     }
     
+    //Who question always ask PERSON
+    //[12] who/whom --> PERSON
+    //[13] when --> TIME/DATE
+    //[14] where/what place --> LOCATION
+    //[15] what time (of day) --> TIME
+    //[16] what day (of the week) --> DAY
+    //[17] what/which month --> MONTH
+    //[18] what age/how old --> AGE
+    //[19] what brand --> PRODUCT
+    //[20] what --> NAME
+    //[21] how far/tall/high --> LENGTH
+    //[22] how large/hig/small --> AREA
+    //[23] how heavy --> WEIGHT
+    //[24] how rich --> MONEY
+    //[25] how often --> FREQUENCY
+    //[26] how many --> NUMBER
+    //[27] how long --> LENGTH/DURATION
+    //[28] why/for what --> REASON
     public boolean isWho(HashMap<String,String> tagged,ArrayList<String> sentence){
         if(sentence.contains("who") || sentence.contains("Who"))
             return true;
@@ -17,24 +35,28 @@ public class QuestionClassification {
         return false;
     }
     
+    //Where always ask for location
     public boolean isWhere(HashMap<String,String> tagged,ArrayList<String> sentence){
         if(sentence.contains("where") || sentence.contains("Where"))
             return true;
         return false;
     }
     
+    //Why always ask for reason
     public boolean isWhy(HashMap<String,String> tagged,ArrayList<String> sentence){
         if(sentence.contains("why") || sentence.contains("Why"))
             return true;
         return false;
     }
     
+    //When always ask for time
     public boolean isWhen(HashMap<String,String> tagged,ArrayList<String> sentence){
         if(sentence.contains("when") || sentence.contains("When"))
             return true;
         return false;
     }
     
+   
     public boolean isHow(HashMap<String,String> tagged,ArrayList<String> sentence){
         if(sentence.contains("how") || sentence.contains("How"))
             return true;
